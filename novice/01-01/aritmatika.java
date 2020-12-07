b14a94db4b694fb78a802d591d22fcae69a627e5
Evan Mahendra <evan.mahendra95@gmail.com>
2020-12-07 17:44:19 +0700

    push 01-01 & kasus

---
+++ novice/01-01/aritmatika.java
@@ -0,0 +1,30 @@
+public class aritmatika {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        
+        int result = x + y;
+
+        System.out.println("x + y = " + result);
+
+        result = x - y;
+        System.out.println("x - y = " + result);
+
+        result = x * y;
+        System.out.println("x * y = " + result);
+
+        result = y / x;
+        System.out.println("y / x = " + result);
+
+        result = x % 3;
+        System.out.println("x % 3 = " + result);
+    }
+}
+// macam-macam tipe var
+// byte, short, int, Long, Char, boolean, float, double
+// contohnya :
+// int topi;
+// topi = 200;
+
+// penggunaan konstanta :
+// final int topi = 100;
