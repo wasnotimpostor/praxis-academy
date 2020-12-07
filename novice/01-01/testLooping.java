17ed1f6ea0abcb1ead7d288189d6051c8a7da98b
wasnotimpostor <74591201+wasnotimpostor@users.noreply.github.com>
2020-12-07 17:56:31 +0700

    Delete testLooping.java

--- novice/01-01/testLooping.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class testLooping {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-    }    
-}
